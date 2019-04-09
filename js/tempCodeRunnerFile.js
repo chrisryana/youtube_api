b7766e899d53cd664e1a5ed5cfe0a920aed443ea
christofer1501 <christofer15@yandex.ru>
2019-04-09 11:04:42 +0300

    my first commit

---
+++ js/tempCodeRunnerFile.js
@@ -0,0 +1,11 @@
+const switcher = document.querySelector('#cbx'),
+      more = document.querySelector('.more'),
+      modal = document.querySelector('.modal'),
+      videos = document.querySelectorAll('.videos__item');
+let player;
+
+function calc(a, b) {
+  return a + b;
+}
+
+console.log(calc(4, 5));
